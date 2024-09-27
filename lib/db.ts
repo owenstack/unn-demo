ee561e44959d867c0ea07be11712615fdc52530d
owenEEfobi <jenukohaefobi@gmail.com>
2024-09-27 09:14:37 -0600

    added libsql: update 5 files

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
+
+const dbUrl = process.env.TURSO_DATABASE_URL;
+const token = process.env.TURSO_AUTH_TOKEN;
+
+if (!dbUrl || !token) throw new Error("Missing Credentials");
+
+const libsql = createClient({
+	url: dbUrl,
+	authToken: token,
+});
+
+const adapter = new PrismaLibSQL(libsql);
 
 const prismaClientSingleton = () => {
-	return new PrismaClient();
+	return new PrismaClient({ adapter });
 };
 
 declare global {
