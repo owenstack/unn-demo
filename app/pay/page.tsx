b8773d4a47a3ae187b7d25fe34f1b149742e6189
owenEEfobi <jenukohaefobi@gmail.com>
2024-09-29 11:16:22 -0600

    changed params back to searchParams: update page.tsx

--- app/pay/page.tsx
+++ app/pay/page.tsx
@@ -1,5 +1,8 @@
 import { PayForm } from "@/components/pay-form";
 
-export default function Page({ params }: { params: { r: string } }) {
-	return <PayForm rrr={params.r} />;
+export default function Page({
+	searchParams,
+}: { searchParams: { r: string } }) {
+	const rrr = searchParams.r;
+	return <PayForm rrr={rrr} />;
 }
